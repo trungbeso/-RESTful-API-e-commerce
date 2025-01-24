95ec5d4030466b641d3eee52ac2cf3bcaf3b5b02
trungnq555 <trungnq555gmail.com>
2025-01-24 16:13:23 +0700

    create base for Security function

--- src/main/java/com/trungbeso/dreamshops/exception/ProductNotFoundException.java
+++ src/main/java/com/trungbeso/dreamshops/exception/ProductNotFoundException.java
@@ -1,7 +1,5 @@
 package com.trungbeso.dreamshops.exception;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
 public class ProductNotFoundException extends RuntimeException {
 	public ProductNotFoundException(String message) {
 		super(message);
---
+++ src/main/java/com/trungbeso/dreamshops/security/user/ShopUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.trungbeso.dreamshops.security.user;
+
+import com.trungbeso.dreamshops.exception.ResourceNotFoundException;
+import com.trungbeso.dreamshops.models.User;
+import com.trungbeso.dreamshops.repositories.IUserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
+public class ShopUserDetailsService implements UserDetailsService {
+
+	IUserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = Optional.of(userRepository.findByEmail(email)).orElseThrow(() -> new UsernameNotFoundException(
+			  "User name not found"));
+		return ShopUserDetails.buildUserDetails(user);
+	}
+}
